0bf134e4cab28445b74b0954ba835ed3a87e758b
Daniil Salamatin <salamatindanya21@gmail.com>
2021-03-25 17:49:02 +0300

    Complete tasks number 1 and 2, the third task in revision
    For 3 tasks it is necessary to refine the situation when the number contains the value
    11, 12, etc.

---
+++ 1/app.js
@@ -0,0 +1,8 @@
+// Написать рекурсивную функцию возведения в степень. На вход принимать число и его степень, на выходе выдавать рассчитанное значение.
+"use strict";
+
+function pow(number, power) {
+  return power - 1 ? number * pow(number, power - 1) : number;
+}
+
+console.log(pow(5, 4));
---
+++ 2/app.js
@@ -0,0 +1,18 @@
+// Написать приложение, получающее массив с вложенными массивами и возвращающее его “плоскую” версию. Встроенный метод массивов flat использовать нельзя.
+"use strict";
+
+const array = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+
+function flattenArray(array) {
+  let flatArray = [];
+  array.forEach((elem) => {
+    if (Array.isArray(elem)) {
+      flatArray = flatArray.concat(flattenArray(elem));
+    } else {
+      flatArray.push(elem);
+    }
+  });
+  return flatArray;
+}
+
+console.log(flattenArray(array));
